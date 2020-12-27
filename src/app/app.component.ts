c1c5ecb57ad638d5bf9a4b5f12faf706147b736e
Francisco Vaz <ftomasvaz@gmail.com>
2020-12-27 18:08:56 +0000

    counter service to log how many times pass from active to inactive and reverse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { CounterService } from './services/counter.service';
 import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UsersService]
 })
 export class AppComponent implements OnInit {
   activeUsers: string[] = [];
   inactiveUsers: string[] = [];
 
-  constructor(private usersService: UsersService) {}
+  counterActives: number;
+  counterInactives: number;
+
+  constructor(private usersService: UsersService, private counterService: CounterService) {}
 
   ngOnInit() {
     this.activeUsers = this.usersService.activeUsers;
     this.inactiveUsers = this.usersService.inactiveUsers;
+    this.counterActives = this.counterService.inactiveToActive;
+    this.counterInactives = this.counterService.activeToInactive;
   }
-
-  /* onSetToInactive(id: number) {
-    this.inactiveUsers.push(this.activeUsers[id]);
-    this.activeUsers.splice(id, 1);
-  }
-
-  onSetToActive(id: number) {
-    this.activeUsers.push(this.inactiveUsers[id]);
-    this.inactiveUsers.splice(id, 1);
-  } */
 }
