ab63feecc6d599bea74461d074d058ce818771be
Francisco Vaz <ftomasvaz@gmail.com>
2020-12-27 10:50:53 +0000

    create usersService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  activeUsers = ['Max', 'Anna'];
-  inactiveUsers = ['Chris', 'Manu'];
+export class AppComponent implements OnInit {
+  activeUsers: string[] = [];
+  inactiveUsers: string[] = [];
 
-  onSetToInactive(id: number) {
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit() {
+    this.activeUsers = this.usersService.activeUsers;
+    this.inactiveUsers = this.usersService.inactiveUsers;
+  }
+
+  /* onSetToInactive(id: number) {
     this.inactiveUsers.push(this.activeUsers[id]);
     this.activeUsers.splice(id, 1);
   }
@@ -17,5 +25,5 @@ export class AppComponent {
   onSetToActive(id: number) {
     this.activeUsers.push(this.inactiveUsers[id]);
     this.inactiveUsers.splice(id, 1);
-  }
+  } */
 }
